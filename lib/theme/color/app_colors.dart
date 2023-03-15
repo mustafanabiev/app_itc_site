3490b5dd06e59f211a8678e77a4225307df98968
mustafanabiev <nabievvv02@gmail.com>
2023-03-15 16:13:33 +0600

    added text, textstyle, colord

--- lib/theme/color/app_colors.dart
+++ lib/theme/color/app_colors.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color red = Colors.red;
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+  static const Color red = Color(0xffFF693A);
+  static const Color blue = Color(0xff049fE3);
+  static const Color redder = Color(0xffFEF5F1);
+  static const Color grey = Color(0xffD6D6D6);
 }
---
+++ lib/theme/text_styles/text_styles.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const TextStyle appBar = TextStyle(
+    fontSize: 20,
+  );
+}
